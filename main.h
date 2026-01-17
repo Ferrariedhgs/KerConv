84e7f6acf40a675c2b5ec416587c41d8f14fef7a
Buia Alexandru <45429773+Ferrariedhgs@users.noreply.github.com>
2026-01-17 12:42:05 +0200

    Refactor main.h to use double for kernel functions

--- main.h
+++ main.h
@@ -2,6 +2,7 @@
 #define __MAIN_H__
 
 #include <windows.h>
+#include <math.h>
 #include <string>
 
 #ifdef KERCONV_EXPORT
@@ -37,15 +38,15 @@ typedef enum
 }kernelVariation;
 
 int* KERCONV_API InitKernel(const int x=3);
-bool KERCONV_API FreeKernel(int** kernel);
-bool KERCONV_API CreateKernel(const char* type, int** kernel, kernelType ktype, kernelVariation variation, const int x=3);
-bool KERCONV_API ConvKernel(int*** image, int** destination, const int imgx, const int imgy, const int imgz, const int** kernel, const int kerx=3);
+bool KERCONV_API FreeKernel(double** kernel);
+bool KERCONV_API CreateKernel(const char* type, double** kernel, kernelType ktype, kernelVariation variation, const int x=3);
+bool KERCONV_API ConvKernel(int*** image, int** destination, const int imgx, const int imgy, const int imgz, const double** kernel, const int kerx=3);
 bool KERCONV_API MakeMono(int*** image, const int imgx, const int imgy, int** destination);
-bool KERCONV_API CreateKernelSobel(int** kernel, kernelVariation variation, const int x=3);
-bool KERCONV_API CreateKernelScharr(int** kernel, kernelVariation variation, const int x=3);
-bool KERCONV_API CreateKernelMedian(int** kernel, const int x=3);
-bool KERCONV_API CreateKernelGaussian(int** kernel, double sigma, const int x=3);
-bool KERCONV_API CreateKernelShift(int** kernel, kernelVariation direction, const int x=3);
+bool KERCONV_API CreateKernelSobel(double** kernel, kernelVariation variation, const int x=3);
+bool KERCONV_API CreateKernelScharr(double** kernel, kernelVariation variation, const int x=3);
+bool KERCONV_API CreateKernelMedian(double** kernel, const int x=3);
+bool KERCONV_API CreateKernelGaussian(double** kernel, double sigma=1, const int x=3);
+bool KERCONV_API CreateKernelShift(double** kernel, kernelVariation direction, const int x=3);
 
 
 #ifdef __cplusplus
