0f42a0daeaf1f2575100b68baf9cf9395f77510e
Buia Alexandru <45429773+Ferrariedhgs@users.noreply.github.com>
2026-01-19 15:02:37 +0200

    Refactor kernel functions to use linear arrays

--- main.h
+++ main.h
@@ -37,16 +37,16 @@ typedef enum
     Right
 }kernelVariation;
 
-int* KERCONV_API InitKernel(const int x=3);
-bool KERCONV_API FreeKernel(double** kernel);
-bool KERCONV_API CreateKernel(const char* type, double** kernel, kernelType ktype, kernelVariation variation, const int x=3);
-bool KERCONV_API ApplyKernel(int** image, int** destination, const int imgx, const int imgy, const double** kernel, const int kerx=3);
-bool KERCONV_API MakeMono(int*** image, const int imgx, const int imgy, int** destination);
-bool KERCONV_API CreateKernelSobel(double** kernel, kernelVariation variation, const int x=3);
-bool KERCONV_API CreateKernelScharr(double** kernel, kernelVariation variation, const int x=3);
-bool KERCONV_API CreateKernelMedian(double** kernel, const int x=3);
-bool KERCONV_API CreateKernelGaussian(double** kernel, double sigma=1, const int x=3);
-bool KERCONV_API CreateKernelShift(double** kernel, kernelVariation direction, const int x=3);
+double* KERCONV_API InitKernel(const int x=3);
+bool KERCONV_API FreeKernel(double* kernel);
+bool KERCONV_API CreateKernel(double* kernel, kernelType ktype, kernelVariation variation, const int x=3);
+bool KERCONV_API ApplyKernel(int* image, int* destination, const int imgx, const int imgy, const double* kernel, const int kerx=3);
+bool KERCONV_API MakeMono(int* image, const int imgx, const int imgy, int* destination);
+bool KERCONV_API CreateKernelSobel(double* kernel, kernelVariation variation, const int x=3);
+bool KERCONV_API CreateKernelScharr(double* kernel, kernelVariation variation, const int x=3);
+bool KERCONV_API CreateKernelMedian(double* kernel, const int x=3);
+bool KERCONV_API CreateKernelGaussian(double* kernel, double sigma=1, const int x=3);
+bool KERCONV_API CreateKernelShift(double* kernel, kernelVariation direction, const int x=3);
 
 
 #ifdef __cplusplus
