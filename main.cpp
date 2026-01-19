2d8f6f2568ca50d67014775e916ed453662f97a4
Buia Alexandru <45429773+Ferrariedhgs@users.noreply.github.com>
2026-01-19 15:02:14 +0200

    Refactor kernel functions to use linear arrays
    Refactor kernel management functions to use linear arrays and update memory allocation.

--- main.cpp
+++ main.cpp
@@ -1,28 +1,30 @@
 #include "main.h"
 
-int* KERCONV_API InitKernel(int x)
+double* KERCONV_API InitKernel(int x)
 {
     if (x <= 0 || x % 2 == 0)
         return NULL;
 
-    int* kernel = (int*)malloc((size_t)x * x * sizeof(double));
+    double* kernel = (double*) malloc((size_t)(x * x) * sizeof(double));
     if (!kernel)
         return NULL;
 
     return kernel;
 }
 
-bool KERCONV_API FreeKernel(double** kernel)
+bool KERCONV_API FreeKernel(double* kernel)
 {
     if (!kernel || !*kernel)
         return 0;
-    free(*kernel);
-    *kernel = NULL;
+
+    free(kernel);
+
+    //*kernel = NULL;
     return 1;
 }
 
 
-bool KERCONV_API CreateKernel(const char* type, double** kernel, kernelType ktype, kernelVariation variation, const int x)
+bool KERCONV_API CreateKernel(double* kernel, kernelType ktype, kernelVariation variation, const int x)
 {
     if(ktype==Sobel) CreateKernelSobel(kernel,variation,x);
     else if(ktype==Scharr) CreateKernelScharr(kernel,variation,x);
@@ -37,7 +39,7 @@ bool KERCONV_API CreateKernel(const char* type, double** kernel, kernelType ktyp
     return 1;
 }
 
-bool KERCONV_API ApplyKernel(int** image, int** destination, const int imgx, const int imgy, const double** kernel, const int kerx)
+bool KERCONV_API ApplyKernel(int* image, int* destination, const int imgx, const int imgy, const double* kernel, const int kerx)
 {
     if(kerx%2==0)
     {
@@ -52,25 +54,25 @@ bool KERCONV_API ApplyKernel(int** image, int** destination, const int imgx, con
             double sum=0;
             for(int y=-padding; y<=padding; y++)
                 for(int x=-padding; x<=padding; x++)
-                    sum+=kernel[y+padding][x+padding]*image[y+h][x+w];
+                    sum+=kernel[(y+padding)*kerx + x+padding]*image[(y+h)*imgx + (x+w)];
             if(sum>255) sum=255;
             if(sum<0) sum=0;
-            destination[h][w]=sum;
+            destination[h*imgx + w]=sum;
         }
 
     return 1;
 }
 
-bool KERCONV_API MakeMono(const int*** image, const int imgx, const int imgy, int** destination)
+bool KERCONV_API MakeMono(const int* image, const int imgx, const int imgy, int* destination)
 {
     //RGB ITU-R BT.601
-    for(int i=0;i<imgx;i++)
-        for(int j=0;j<imgy;j++)
-            destination[i][j]=(int)(0.299*image[i][j][0]+0.587*image[i][j][1]+0.114*image[i][j][2]);
+    for(int i=0;i<imgy;i++)
+        for(int j=0;j<imgx;j++)
+            destination[i*imgx + j]=(int)(0.299*image[(i*imgx+j)*3] + 0.587*image[(i*imgx+j)*3+1] + 0.114*image[(i*imgx+j)*3+2] + 0.5);
     return 1;
 }
 
-bool KERCONV_API CreateKernelSobel(double** kernel, kernelVariation variation, const int x)
+bool KERCONV_API CreateKernelSobel(double* kernel, kernelVariation variation, const int x)
 {
     if(x%2==0)
     {
@@ -83,7 +85,7 @@ bool KERCONV_API CreateKernelSobel(double** kernel, kernelVariation variation, c
     //binomial coefficients
     binomial[0]=1;
     for (int i=1; i<x; i++) {
-        binomial[i] = binomial[i - 1] * (x-i)/i;
+        binomial[i] = binomial[i-1] * (x-i)/i;
     }
 
     //derivative
@@ -95,14 +97,14 @@ bool KERCONV_API CreateKernelSobel(double** kernel, kernelVariation variation, c
     {
         for (int h=0; h<x; h++)
             for (int w=0; w<x; w++)
-                kernel[h][w] = binomial[h] * derivative[w];
+                kernel[h*x + w] = binomial[h] * derivative[w];
 
     }
     else if(variation==Horizontal)
     {
         for (int h=0; h<x; h++)
             for (int w=0; w<x; w++)
-                kernel[h][w] = binomial[w] * derivative[h];
+                kernel[h*x + w] = binomial[w] * derivative[h];
     }
     else
     {
@@ -113,7 +115,7 @@ bool KERCONV_API CreateKernelSobel(double** kernel, kernelVariation variation, c
     return 1;
 }
 
-bool KERCONV_API CreateKernelScharr(double** kernel, kernelVariation variation, const int x)
+bool KERCONV_API CreateKernelScharr(double* kernel, kernelVariation variation, const int x)
 {
     if(x%2==0)
     {
@@ -129,9 +131,9 @@ bool KERCONV_API CreateKernelScharr(double** kernel, kernelVariation variation, 
         {
             if(w==x/2) continue;
 
-            if(h==x/2) kernel[h][w]*=5;
+            if(h==x/2) kernel[h*x + w]*=5;
 
-            else kernel[h][w]*=3;
+            else kernel[h*x + w]*=3;
         }
     }
     else if(variation==Horizontal)
@@ -141,9 +143,9 @@ bool KERCONV_API CreateKernelScharr(double** kernel, kernelVariation variation, 
         {
             if(h==x/2) continue;
 
-            if(w==x/2) kernel[h][w]*=5;
+            if(w==x/2) kernel[h*x + w]*=5;
 
-            else kernel[h][w]*=3;
+            else kernel[h*x + w]*=3;
         }
     }
     else
@@ -154,21 +156,21 @@ bool KERCONV_API CreateKernelScharr(double** kernel, kernelVariation variation, 
     return 1;
 }
 
-bool KERCONV_API CreateKernelMedian(double** kernel, const int x)
+bool KERCONV_API CreateKernelMedian(double* kernel, const int x)
 {
     if(x%2==0)
     {
         fprintf(stderr, "Kernel size must be odd\n");
         return 0;
     }
-    int med=1/(x*x);
+    double med=1.0/(x*x);
     for(int i=0;i<x;i++)
         for(int j=0;j<x;j++)
-            kernel[i][j]=med;
+            kernel[i*x + j]=med;
     return 1;
 }
 
-bool KERCONV_API CreateKernelGaussian(double** kernel, double sigma, const int x)
+bool KERCONV_API CreateKernelGaussian(double* kernel, double sigma, const int x)
 {
     if(x%2==0)
     {
@@ -184,7 +186,7 @@ bool KERCONV_API CreateKernelGaussian(double** kernel, double sigma, const int x
         for (int w=-half; w<=half; w++)
         {
             double value = exp(-(w*w + h*h) / (2 * sigma * sigma));
-            kernel[h + half][w + half] = value;
+            kernel[(h+half)*x + (w+half)]=value;
             sum += value;
         }
     }
@@ -192,12 +194,12 @@ bool KERCONV_API CreateKernelGaussian(double** kernel, double sigma, const int x
     //normalize
     for (int h=0; h<x; h++)
         for (int w=0; w<x; w++)
-            kernel[h][w] /= sum;
+            kernel[h*x + w] /= sum;
 
     return 1;
 }
 
-bool KERCONV_API CreateKernelShift(double** kernel, kernelVariation direction, const int x)
+bool KERCONV_API CreateKernelShift(double* kernel, kernelVariation direction, const int x)
 {
     if(x%2==0)
     {
@@ -207,23 +209,23 @@ bool KERCONV_API CreateKernelShift(double** kernel, kernelVariation direction, c
 
     for(int h=0; h<x; h++)
         for(int w=0; w<x; w++)
-            kernel[h][w]=0;
+            kernel[h*x + w]=0;
 
     if(direction==Up)
     {
-        kernel[0][x/2]=1;
+        kernel[x/2]=1;
     }
     else if(direction==Down)
     {
-        kernel[x-1][x/2]=1;
+        kernel[(x-1)*x + x/2]=1;
     }
     else if(direction==Left)
     {
-        kernel[x/2][0]=1;
+        kernel[(x/2)*x]=1;
     }
     else if(direction==Right)
     {
-        kernel[x/2][x-1]=1;
+        kernel[(x/2)*x + (x-1)]=1;
     }
     else
     {
